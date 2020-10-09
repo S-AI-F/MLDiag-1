ccc7aad0b7f7d7c27e10f37b281a7528f92a1c41
AI-MEN <aymen.shabou@gmail.com>
2020-10-10 00:55:54 +0200

    add docs + image example

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,6 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'rinoh.frontend.sphinx',
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
